f0d61f3d5770770f0dfaac804996ce161179f6d5
Carlos Alves <carlos.alves.one@gmail.com>
2022-05-01 09:56:00 +0100

    Create flat class extends dwelling class - create constructor matching super

--- src/Dwelling.java
+++ src/Dwelling.java
@@ -33,5 +33,5 @@ public class Dwelling {
     public String getCountry() {return country;}
     public void setCountry(String country) {this.country = country;}
 
-    
+
 }
---
+++ src/Flat.java
@@ -0,0 +1,18 @@
+
+/*
+ * Goldsmiths University of London
+ * Author...: Carlos Manuel de Oliveira Alves
+ * Student..: cdeol003
+ * Created..: 01/05/2022
+ */
+
+/* declare class Flat and extends Dwelling */
+public class Flat extends Dwelling{
+
+    /* create constructor matching super */
+    public Flat(int number, String street, String town, String country) {
+        super(number, street, town, country);
+    }
+
+    
+}
