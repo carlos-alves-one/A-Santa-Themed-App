8d0e99a16e8acf1bc5d57b689769fca343163b40
Carlos Alves <carlos.alves.one@gmail.com>
2022-05-02 08:03:52 +0100

    Create class Child: instance variables and construction function

---
+++ src/Child.java
@@ -0,0 +1,27 @@
+
+/*
+ * Goldsmiths University of London
+ * Author...: Carlos Manuel de Oliveira Alves
+ * Student..: cdeol003
+ * Created..: 02/05/2022
+ */
+
+/* declare class child */
+public class Child {
+
+    /* declare instances variables */
+    private String name;
+    private int age;
+    private Dwelling address;
+    private String wishList;
+    private int naughtinessRating;
+
+    /* declare the construction function for this class */
+    public Child(String name, int age, Dwelling address, String wishList, int naughtinessRating) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+        this.wishList = wishList;
+        this.naughtinessRating = naughtinessRating;
+    }
+}
